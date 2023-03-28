bbc6ac7eec520ee938e8d284c7e449ac648a3549
jjhkie <83710866+jjhkie@users.noreply.github.com>
2023-03-28 10:42:51 +0900

    Initial Commit

---
+++ IosBaseInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosBaseInfo
+//
+//  Created by 김진혁 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
